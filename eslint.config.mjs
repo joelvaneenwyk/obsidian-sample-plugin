9d1d9fd2ee4941491d201605c605443581b860dc
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2024-07-03 15:40:29 -0700

    Remove superfluous config from eslint and remove eslintignore
    Signed-off-by: Joel Van Eenwyk <joel.vaneenwyk@gmail.com>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,36 +3,20 @@
 import typescriptEslint from '@typescript-eslint/eslint-plugin';
 import globals from 'globals';
 import tsParser from '@typescript-eslint/parser';
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
 import js from '@eslint/js';
-import { FlatCompat } from '@eslint/eslintrc';
 import eslintConfigPrettier from 'eslint-config-prettier';
 import eslintPluginPrettier from 'eslint-plugin-prettier';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all
-});
-
 export default [
   {
-    ignores: ['**/node_modules/', '**/main.js']
+    ignores: ['**/node_modules/']
   },
-  ...compat.extends(
-    'eslint:recommended',
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended'
-  ),
+  js.configs.recommended,
   {
     plugins: {
       '@typescript-eslint': typescriptEslint,
       prettier: eslintPluginPrettier
     },
-
     languageOptions: {
       globals: {
         ...globals.node
@@ -42,7 +26,6 @@ export default [
       ecmaVersion: 5,
       sourceType: 'module'
     },
-
     rules: {
       'no-unused-vars': 'off',
 
@@ -55,7 +38,19 @@ export default [
 
       '@typescript-eslint/ban-ts-comment': 'off',
       'no-prototype-builtins': 'off',
-      '@typescript-eslint/no-empty-function': 'off'
+      '@typescript-eslint/no-empty-function': 'off',
+
+      'prettier/prettier': 'error'
+    }
+  },
+  {
+    files: ['dist/**/*.js'],
+    languageOptions: {
+      globals: {
+        ...globals.browser
+      },
+      ecmaVersion: 5,
+      sourceType: 'module'
     }
   },
   eslintConfigPrettier
